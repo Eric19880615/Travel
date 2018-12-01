bb69dfcb585c63ff1b1f106e24cbe07f43a39aff
Eric19880615 <ever985570792@163.com>
2018-12-01 15:24:42 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="list">
+    <li class="item">a</li>
+    <li class="item">a</li>
+    <li class="item">a</li>
+    <li class="item">a</li>
+    <li class="item">a</li>
+    <li class="item">a</li>
+    <li class="item">a</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "CityAlphabet"
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "~stylus/variables.styl"
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    align-items center
+    position absolute
+    width .4rem
+    top 1.58rem
+    bottom 0
+    right 0
+    .item
+      color $bgColor
+      line-height .4rem
+
+
+
+</style>
