27024dd798e168400c942687de840275d8de2a78
Eric19880615 <ever985570792@163.com>
2018-12-02 12:25:02 +0800

    async component

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
-
 
 Vue.use(Router);
-
+/*异步组件适合大型项目使用*/
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => ('@/pages/home/Home')
     },
     {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => ('@/pages/home/City')
     },
     {
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => ('@/pages/detail/Detail')
     }
   ],
   scrollBehavior(to, from, savedPosition) {
